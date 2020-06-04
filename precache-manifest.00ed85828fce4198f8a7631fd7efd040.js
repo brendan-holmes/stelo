8fea8565096052933d28576079f19e0ac847530f
Brendan Holmes <brendholmes@gmail.com>
2020-06-04 22:43:00 +1000

    Updates

---
+++ precache-manifest.00ed85828fce4198f8a7631fd7efd040.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbfe4ef15d7191acacf28a12cb35cda9",
+    "url": "/stelo/index.html"
+  },
+  {
+    "revision": "48cb952c21acb5123948",
+    "url": "/stelo/static/css/main.c3139867.chunk.css"
+  },
+  {
+    "revision": "3ea1d786c14e558f0977",
+    "url": "/stelo/static/js/2.33cec753.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stelo/static/js/2.33cec753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48cb952c21acb5123948",
+    "url": "/stelo/static/js/main.4205f002.chunk.js"
+  },
+  {
+    "revision": "b9562826582610f098ad",
+    "url": "/stelo/static/js/runtime-main.4912353b.js"
+  },
+  {
+    "revision": "6f93d587d4b9972cdb7993963d43112d",
+    "url": "/stelo/static/media/LEMONMILK-BoldItalic.6f93d587.otf"
+  }
+]);
